32f4b0eb6ea49040a61c06ac957ca24676479b61
Geandre Miranda <geandremiranda.ms@gmail.com>
2022-03-14 12:47:45 -0400

    Clean

--- src/App.vue
+++ src/App.vue
@@ -15,18 +15,8 @@ export default {
       new CompClass({
         propsData: { item: [] },
       }).$mount();
+      //
     });
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
